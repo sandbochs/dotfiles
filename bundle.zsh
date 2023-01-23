9f401ec925af44eda280647e929d1a1608ff0095
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-01-23 10:33:09 -0800

    enable zsh completions and suggestions

--- bundle.zsh
+++ bundle.zsh
@@ -2,8 +2,8 @@ antigen use oh-my-zsh
 
 antigen bundle git
 
-# antigen bundle zsh-users/zsh-completions
-# antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle jeffreytse/zsh-vi-mode
 
